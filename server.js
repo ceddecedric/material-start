569a2f8612e0fcc95c47ab7d92f1eeed728b55e4
kevin DOH <kevin DOH>
2017-05-07 00:17:48 +0200

    server okk

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static('./app'));
+app.use(express.static('app'));
 
 app.listen(3000, () => {
     console.log('http://localhost:3000');
