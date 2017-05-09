cdb9bc38f8a66cd365a1e209ed69cd7e2a140339
kevin DOH <kevin DOH>
2017-05-09 12:10:28 +0200

    server modifié

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
+var path = require('path');
 var express = require('express');
+promise = require('bluebird');
+mongoose = promise.promisifyAll(require('mongoose'));
+fs = promise.promisifyAll(require('fs'));
+
+
 var app = express();
 
 app.use(express.static('app'));
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.listen(3000, () => {
     console.log('http://localhost:3000');
