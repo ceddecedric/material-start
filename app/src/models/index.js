de3425443b29ea645d4e65f9ce14075cbe5e31f6
kevin DOH <kevin DOH>
2017-05-09 17:14:20 +0200

    schema avance

--- app/src/models/index.js
+++ app/src/models/index.js
@@ -1,8 +1,15 @@
 var users = require('../schema/users');
 var invitationTemps = require('../schema/invitations_temp');
+var avoirAmis = require('../schema/avoirAmis');
+var groupes = require('../schema/groupe');
+var membreDes = require('../schema/membreDe');
+var messageGroupes = require('../schema/messagesGroupe');
+
 
 exports.User = mongoose.model('Users', users.schemaUsers);
 exports.InvitationTemp = mongoose.model('InvitationTemps', invitationTemps.schemaInvitationtemps);
-
-var avoirAmis = require('../schema/avoirAmis')
 exports.AvoirAmi = mongoose.model('AvoirAmis', avoirAmis.schemaAvoirAmis);
+exports.Groupe = mongoose.model('Groupes', groupes.schemaGroupes);
+exports.MembreDe = mongoose.model('MembreDes', membreDes.schemaMembreDes);
+exports.MessageGroupes = mongoose.model('MessageGroupes', messageGroupes.schemaMessagesGroupes);
+
---
+++ app/src/schema/messagesGroupe.js
@@ -0,0 +1,5 @@
+exports.schemaMessagesGroupes = new mongoose.Schema({
+    idGroupe: {type: String, maxlength: 60},
+    messageText: {type: String, maxlength: 254},
+    time: {type: Date}
+});
