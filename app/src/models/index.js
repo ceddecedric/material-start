7f2465e1c60515f381fcedca5d92955ce038dca7
kevin DOH <kevin DOH>
2017-05-09 15:55:33 +0200

    schema en cours

--- app/src/models/index.js
+++ app/src/models/index.js
@@ -1,3 +1,6 @@
 var users = require('../schema/users');
+var avoirAmis = require('../schema/avoirAmis')
+
 
 exports.User = mongoose.model('Users', users.schemaUsers);
+exports.AvoirAmi = mongoose.model('AvoirAmis', avoirAmis.schemaAvoirAmis);
