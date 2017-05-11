e2e43e3ad30710058e971b082545ec604eade8f5
ceddecedric <cedric.amegan@gmail.com>
2017-05-12 01:44:07 +0200

    conf de server io ajoute a serve mais serve ne fonctionne pas donc je ne peux pas verifier lenvoie de msg

--- app/src/users/components/details/UserDetails.js
+++ app/src/users/components/details/UserDetails.js
@@ -8,3 +8,15 @@ export default {
     controller       : [ '$mdBottomSheet', '$log', UserDetailsController ]
   }
 };
+
+$(function () {
+  var socket = io();
+  $('form').submit(function(){
+    socket.emit('chat message', $('#m').val());
+    $('#m').val('');
+    return false;
+  });
+  socket.on('chat message', function(msg){
+    $('#messages').append($('<li>').text(msg));
+  });
+});
