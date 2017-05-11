7d6479528d83c6453a145507603ad16789778ea9
ceddecedric <cedric.amegan@gmail.com>
2017-05-12 01:47:02 +0200

    conf de server io ajoute a serve mais serve ne fonctionne pas donc je ne peux pas verifier lenvoie de msg

--- app/src/users/components/details/UserDetails.js
+++ app/src/users/components/details/UserDetails.js
@@ -9,14 +9,14 @@ export default {
   }
 };
 
-$(function () {
-  var socket = io();
-  $('form').submit(function(){
-    socket.emit('chat message', $('#m').val());
-    $('#m').val('');
-    return false;
-  });
-  socket.on('chat message', function(msg){
-    $('#messages').append($('<li>').text(msg));
-  });
-});
+// $(function () {
+//   var socket = io();
+//   $('form').submit(function(){
+//     socket.emit('chat message', $('#m').val());
+//     $('#m').val('');
+//     return false;
+//   });
+//   socket.on('chat message', function(msg){
+//     $('#messages').append($('<li>').text(msg));
+//   });
+// });
