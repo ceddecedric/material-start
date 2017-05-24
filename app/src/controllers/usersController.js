3278bc0bc24e75bbce8cfbf2fcbf25790aa46bb8
elvino 29 <kevindoh26@gmail.com>
2017-05-24 11:23:57 +0200

    register ok

--- app/src/controllers/usersController.js
+++ app/src/controllers/usersController.js
@@ -8,15 +8,39 @@ exports.inscription = function (req, res) {
 };
 
 exports.create = function (req, res) {
-    var returnResponse = function (obj) {
-        res.render('inscription');
-    };
+   var name = req.body.name;
+   var email = req.body.email;
+   var password = req.body.password;
+   var confpass = req.body.confpass;
 
+  //validation
+    req.checkBody('name', 'Name is required').notEmpty();
+    req.checkBody('email', 'email is required').notEmpty();
+    req.checkBody('email', 'email is not valid').isEmail();
+    req.checkBody('password', 'password is required').notEmpty();
+    req.checkBody('confpass', 'passwords do not match').equals(req.body.password);
 
-    models.User(req.body).saveAsync()
-        .then(logLib.logContent)
-        .then(returnResponse);
+    var errors = req.validationErrors();
 
+    if(errors){
+        res.render('inscription', {
+            errors:errors
+        });
+    }else {
+        bcrypt.genSalt(10, function(err, salt) {
+            bcrypt.hash(req.body.password, salt, function(err, hash) {
+                req.body.password = hash;
+                var returnResponse = function (obj) {
+                    req.flash('success_msg', 'You are now registered and can now login');
+                    res.redirect('/login');
+                };
+                models.User(req.body).saveAsync()
+                    .then(logLib.logContent)
+                    .then(returnResponse);
+
+            });
+        });
+    }
 };
 
 exports.login = function (req, res) {
--- server.js
+++ server.js
@@ -24,12 +24,21 @@ mongoose = promise.promisifyAll(require('mongoose'));
 fs = promise.promisifyAll(require('fs'));
 logLib = require('./app/src/lib/log');
 exphbs = require('express-handlebars');
+var cookieParser = require('cookie-parser');
+var expressValidator = require('express-validator');
+var flash = require('connect-flash');
+var session = require('express-session');
+var passport = require('passport');
+var LocalSrategy = require('passport-local').Strategy;
+bcrypt = require('bcryptjs');
 
  app = express();
 
-
+// BodyParser middleware
 var bodyParser = require('body-parser');
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
 
 //config
 app.use(express.static(path.join(__dirname + '/')));
@@ -38,6 +47,41 @@ app.use(express.static(path.join(__dirname + '/')));
  app.engine('handlebars', exphbs({defaultLayout: 'main', layoutsDir: __dirname + '/app/src/views/layouts'}));
  app.set('view engine', 'handlebars');
 mongoose.connect('mongodb://localhost/qwirk_db');
+
+// Express Session
+app.use(session({
+    secret: 'secret',
+    saveUninitialized: true,
+    resave: true
+}));
+
+//express validator
+app.use(expressValidator({
+    errorFormatter: function(param, msg, value) {
+        var namespace = param.split('.')
+            , root    = namespace.shift()
+            , formParam = root;
+
+        while(namespace.length) {
+            formParam += '[' + namespace.shift() + ']';
+        }
+        return {
+            param : formParam,
+            msg   : msg,
+            value : value
+        };
+    }
+}));
+
+//connect flash
+app.use(flash());
+
+app.use(function (req, res, next) {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('errr_msg');
+    res.locals.error = req.flash('error');
+    next();
+})
 
 //import models
 models = require('./app/src/models');
