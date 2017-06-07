1965f4881b728e2a4ddacdb60397e5aff6196077
ceddecedric <cedric.amegan@gmail.com>
2017-06-07 13:28:44 +0200

    modif

--- app/src/schema/messagesGroupe.js
+++ app/src/schema/messagesGroupe.js
@@ -1,5 +1,5 @@
 exports.schemaMessagesGroupes = new mongoose.Schema({
     idGroupe: {type: String, maxlength: 60},
     messageText: {type: String, maxlength: 254},
-    time: {type: Date}
+    time: {type: Date, default: new Date()}
 });
